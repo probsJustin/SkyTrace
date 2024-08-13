c48b16ba78208bf0b858c1bf14f11b305f75dd4c
Justin Hagerty <wheelsmanx@gmail.com>
2024-08-13 11:34:45 -0400

    add admin table configuration

--- apps/adbs/src/app/discord/discord.service.ts
+++ apps/adbs/src/app/discord/discord.service.ts
@@ -3,6 +3,7 @@ import { DiscordDto } from './types/discord.dto';
 import { HttpService } from '@nestjs/axios';
 import { InjectModel } from '@nestjs/sequelize';
 import { InternalConfig } from '../config/types/internal.config.model';
+import { AdminConfig } from '../admin/types/admin.config.model';
 
 @Injectable()
 export class DiscordService {
@@ -10,6 +11,8 @@ export class DiscordService {
     private httpService: HttpService,
     @InjectModel(InternalConfig)
     private readonly internalConfig: typeof InternalConfig,
+    @InjectModel(AdminConfig)
+    private readonly adminConfig: typeof AdminConfig,
     ){
   }
 
@@ -20,7 +23,25 @@ export class DiscordService {
       }
     });    
 
-    const discordWebHookUrl = "https://discord.com/api/webhooks/1272651457069449267/cpEUiJKm3qtMXJMr6056Fm-sPKMsj9nrQKKqQ-f0AU2cQeM3nQR4trRmsnrTMNM1uiQP";
+    const payload = {
+      content: discordRequest.discordMessage,
+      username: "ADBS Exchange",  // Optional: customize the username
+      //avatar_url: "https://path-to-your-avatar-image.png"  // Optional: customize the avatar
+    };
+
+    try {
+      await this.httpService.post(tenantConfiguration.discordWebhook, payload).toPromise();
+      console.log('Message sent successfully');
+    } catch (error) {
+      console.error('Failed to send message via webhook:', error);
+    }
+  }
+  async sendAdminMessage(discordRequest: DiscordDto): Promise<void> {
+    const tenantConfiguration = await this.adminConfig.findOne({
+      where: {
+        tenant: "admin" 
+      }
+    });    
 
     const payload = {
       content: discordRequest.discordMessage,
